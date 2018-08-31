519095d59a23b2c066cc876c521585a03acf032c
Verdana <verdana.cn@gmail.com>
2018-08-31 20:55:52 +0800

    setup fibos env

---
+++ public/node.js
@@ -0,0 +1,19 @@
+var fibos = require('fibos');
+
+fibos.load("http", {
+    "http-server-address": "0.0.0.0:8888"
+});
+fibos.load("chain");
+fibos.load("net", {
+    "p2p-listen-endpoint": "0.0.0.0:9876"
+});
+fibos.load("chain_api");
+fibos.load("history_api");
+fibos.load("producer", {
+    'producer-name': 'eosio',
+    'enable-stale-production': true
+});
+fibos.config_dir = "fibos_config_dir/";
+fibos.data_dir = "fibos_data_dir/";
+
+fibos.start();
